b32df4913c0f04f1d97b12c711973e03ca75a17e
Marlon Ferreira <ferreira.marlon@live.com>
2024-06-03 11:37:47 -0300

    style: estilização da projectpage add responsividade

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,16 +4,14 @@ import Link from "next/link";
 
 const ProjectsPage = () => {
    return (
-      <section className="p-5 flex-col flex gap-10">
+      <section className="p-5 flex-col flex gap-10 container mx-auto">
          <h2 className="text-xl font-semibold text-purple-600">Projetos</h2>
 
          <div className="grid grid-cols-1 gap-5">
             <div className="border-2 rounded-xl shadow-md">
                <Image src="/food.png" alt="food" width={0} height={0} sizes="100vw" className="w-full rounded-2xl object-contain p-2" />
 
-               <p className="p-2 text-xs font-medium text-left">
-                  Desenvolvimento de uma plataforma de delivery de alimentos inspirada no iFood.
-               </p>
+               <p className="p-2 text-xs font-medium text-left">Desenvolvimento de uma plataforma de delivery de alimentos inspirada no iFood.</p>
 
                <div className="flex items-center justify-around p-2 gap-3">
                   <Link
