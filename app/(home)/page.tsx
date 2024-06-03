5c35336d0e46e3c0390ee45c02a01374775493be
Marlon Ferreira <ferreira.marlon@live.com>
2024-06-03 11:37:16 -0300

    style: estilização da homepage add responsividade

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
    return (
       <section className="flex justify-center items-center flex-col p-5 h-[85%]">
          <Image src={"https://avatars.githubusercontent.com/u/105893969?v=4"} alt="profile" width={200} height={200} className="rounded-full mb-16 border-2 border-purple-500" />
-         <p className="text-center text-xl">
+         <p className="text-center text-xl md:text-2xl">
             <span className="text-2xl font-semibold text-purple-600">Olá! 👋</span>
             <br />
             Eu sou Marlon Ferreira, Desenvolvedor Full Stack 💻
