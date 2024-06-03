45cd74be3e0d40ea21a73c111e800b1c0a7809ba
Marlon Ferreira <ferreira.marlon@live.com>
2024-06-03 11:38:36 -0300

    style: estilização da aboutpage add responsividade

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,17 +12,17 @@ const AboutPage = () => {
    };
 
    return (
-      <section className="p-5">
+      <section className="container p-5 mx-auto md:p-20 flex flex-col gap-10">
          <h2 className="text-xl font-semibold text-purple-600">Sobre</h2>
 
-         <p className="mt-10 text-sm leading-6">
+         <p className="text-sm leading-6 md:text-xl">
             Sou Desenvolvedor Full Stack apaixonado por tecnologia e criar códigos. Formado em Desenvolvimento Full Stack e atualmente me graduando em Engenharia de Software. <br />
             Acredito na importância de estar sempre atualizado com as melhores práticas e novas tecnologias. Para isso, desenvolvo projetos pessoais continuamente, visando evoluir meu conhecimento e
             habilidades. Além disso, estou envolvido em um projeto voluntário, onde tenho a oportunidade de aplicar minhas competências técnicas. Além da minha vida profissional, valorizo muito meu
             tempo com minha família e amigos e aproveito as manhãs para surfar, uma atividade que me ajuda a começar o dia com energia e clareza mental.
          </p>
 
-         <div className="flex justify-between items-center mt-10 flex-wrap gap-5">
+         <div className="flex justify-between items-center flex-wrap gap-5">
             <ul className="gap-5 flex items-center">
                <li>
                   <Link href="https://www.linkedin.com/in/jackvr/" target="_blank">
