d3fecf7e8971c9e3ab7e3339251643029816e678
Marlon Ferreira <ferreira.marlon@live.com>
2024-06-19 19:38:52 -0300

    feat: add project bnb and change images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -51,6 +51,27 @@ const ProjectsPage = () => {
                   </Link>
                </div>
             </div>
+
+            <div className="border-2 rounded-xl shadow-md">
+               <Image src="/bnb.png" alt="store" width={0} height={0} sizes="100vw" className="w-full rounded-2xl object-contain p-2" />
+
+               <div className="flex items-center justify-around p-2 gap-3">
+                  <Link
+                     className="text-black bg-purple-400 hover:bg-purple-500 rounded-md flex gap-3 font-medium w-full justify-center items-center p-2 text-center"
+                     href="https://github.com/mfjack/fsw-bnb"
+                     target="_blank">
+                     <Github size={18} />
+                     GitHub
+                  </Link>
+                  {/* <Link
+                     className="text-black disabled: bg-purple-400 hover:bg-purple-500 rounded-md flex gap-3 font-medium w-full justify-center items-center p-2 text-center"
+                     href="https://jack-bnb.vercel.app/"
+                     target="_blank">
+                     <Globe size={18} />
+                     Deploy
+                  </Link> */}
+               </div>
+            </div>
          </div>
       </section>
    );
